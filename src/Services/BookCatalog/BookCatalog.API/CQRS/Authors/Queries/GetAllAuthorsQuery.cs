d69e942e7f7c2e1462a71b76a2d614ee21baafc8
hk0310 <42828125+hk0310@users.noreply.github.com>
2023-06-05 19:44:48 +0700

    Added queries and query handlers for Author

---
+++ src/Services/BookCatalog/BookCatalog.API/CQRS/Authors/Queries/GetAllAuthorsQuery.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using BookCatalog.API.Models;
+using BookCatalog.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BookCatalog.API.CQRS.Authors.Queries;
+
+public class GetAllAuthorsQuery : IRequest<IEnumerable<Author>> { }
+
+public class GetAllAuthorsQueryHandler : IRequestHandler<GetAllAuthorsQuery, IEnumerable<Author>>
+{
+    private readonly IBookCatalogContext _context;
+
+    public GetAllAuthorsQueryHandler(IBookCatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Author>> Handle(GetAllAuthorsQuery request, CancellationToken cancellationToken)
+    {
+        var authors = await _context.Authors.ToListAsync(cancellationToken);
+
+        return authors.AsReadOnly();
+    }
+}
---
+++ src/Services/BookCatalog/BookCatalog.API/CQRS/Authors/Queries/GetAuthorByIdQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using BookCatalog.API.Models;
+using BookCatalog.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.API.CQRS.Authors.Queries;
+
+public class GetAuthorByIdQuery : IRequest<Author>
+{
+    public int Id { get; set; }
+}
+
+public class GetAuthorByIdQueryHandler : IRequestHandler<GetAuthorByIdQuery, Author>
+{
+    private readonly IBookCatalogContext _context;
+
+    public GetAuthorByIdQueryHandler(IBookCatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Author> Handle(GetAuthorByIdQuery request, CancellationToken cancellationToken)
+    {
+        var author = await _context.Authors.FindAsync(request.Id, cancellationToken);
+
+        return author;
+    }
+}
