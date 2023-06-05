e7b4c639bbbe4bfcb44303e802a22e59ae7e9b05
hk0310 <42828125+hk0310@users.noreply.github.com>
2023-06-05 19:47:50 +0700

    Added queries and query handler for Genre

---
+++ src/Services/BookCatalog/BookCatalog.API/CQRS/Genres/Queries/GetAllGenresQuery.cs
@@ -0,0 +1,25 @@
+﻿using BookCatalog.API.Infrastructure;
+using BookCatalog.API.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalog.API.CQRS.Genres.Queries;
+
+public class GetAllGenresQuery : IRequest<IEnumerable<Genre>> { }
+
+public class GetAllGenresQueryHandler : IRequestHandler<GetAllGenresQuery, IEnumerable<Genre>>
+{
+    private readonly IBookCatalogContext _context;
+
+    public GetAllGenresQueryHandler(IBookCatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Genre>> Handle(GetAllGenresQuery request, CancellationToken cancellationToken)
+    {
+        var genres = await _context.Genres.ToListAsync(cancellationToken);
+
+        return genres.AsReadOnly();
+    }
+}
---
+++ src/Services/BookCatalog/BookCatalog.API/CQRS/Genres/Queries/GetGenreByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using BookCatalog.API.Infrastructure;
+using BookCatalog.API.Models;
+using MediatR;
+
+namespace BookCatalog.API.CQRS.Genres.Queries;
+
+public class GetGenreByIdQuery : IRequest<Genre>
+{
+    public int Id { get; set; }
+}
+
+public class GetGenreByIdQueryHandler : IRequestHandler<GetGenreByIdQuery, Genre>
+{
+    private readonly IBookCatalogContext _context;
+
+    public GetGenreByIdQueryHandler(IBookCatalogContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Genre> Handle(GetGenreByIdQuery request, CancellationToken cancellationToken)
+    {
+        var book = await _context.Genres.FindAsync(request.Id, cancellationToken);
+
+        return book;
+    }
+}
