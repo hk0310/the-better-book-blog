b000fbb0c02268e5a5b45695b49854c4de5c9225
hk0310 <42828125+hk0310@users.noreply.github.com>
2023-06-05 19:36:57 +0700

    Created entity models

---
+++ src/Services/BookCatalog/BookCatalog.API/Models/Author.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookCatalog.API.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Introduction { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public Uri Website { get; set; }
+
+    public string Twitter { get; set; }
+
+    public string ImagePath { get; set; }
+
+    public ICollection<Book> Books { get; set; }
+}
---
+++ src/Services/BookCatalog/BookCatalog.API/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace BookCatalog.API.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Synopsis { get; set; }
+
+    public int PageCount { get; set; }
+
+    public DateTime DatePublished { get; set; }
+
+    public string CoverImagePath { get; set; }
+
+    public int AuthorId { get; set; }   
+
+    public Author Author { get; set; }
+
+    public ICollection<Genre> Genres { get; set; } 
+}
