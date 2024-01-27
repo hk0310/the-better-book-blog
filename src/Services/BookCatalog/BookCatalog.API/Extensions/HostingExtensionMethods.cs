3cd8678a44daeb3045fc4de9fe93ff907b960d93
HKhai <hoangkhai.nt@outlook.com>
2024-01-26 19:05:27 -0600

    Restructured some code

---
+++ src/Services/BookCatalog/BookCatalog.API/Extensions/HostingExtensionMethods.cs
@@ -0,0 +1,61 @@
+﻿using BookCatalog.API.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+namespace BookCatalog.API.Extensions
+{
+    public static class HostingExtensionMethods
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers().AddJsonOptions(o =>
+            o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations();
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Book Catalog API",
+                    Description = "A microservice responsible for managing a book catalog, written using ASP.NET Core.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Khai Nguyen",
+                        Email = "hoangkhai.nt@outlook.com"
+                    }
+                });
+            });
+
+            builder.Services.AddDbContext<BookCatalogContext>(options => {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), options => options.EnableRetryOnFailure());
+            });
+
+            builder.Services.AddScoped<IBookCatalogContext>(provider => provider.GetService<BookCatalogContext>());
+
+            builder.Services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            });
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipelines(this WebApplication app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookCatalog.API");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
--- src/Services/BookCatalog/BookCatalog.API/Program.cs
+++ src/Services/BookCatalog/BookCatalog.API/Program.cs
@@ -12,45 +12,11 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        var app = builder
+            .ConfigureServices()
+            .ConfigurePipelines();
 
-        builder.Services.AddControllers().AddJsonOptions(o => 
-            o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.EnableAnnotations();
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "Book Catalog API",
-                Description = "A microservice responsible for managing a book catalog, written using ASP.NET Core.",
-                Contact = new OpenApiContact
-                {
-                    Name = "Khai Nguyen",
-                    Email = "hoangkhai.nt@outlook.com"
-                }
-            });
-        });
-
-        builder.Services.ConfigureServices(builder.Configuration);
         SetUpDirectory();
-
-        var app = builder.Build();
-
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookCatalog.API");
-        });
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
 
         app.Run();
     }
